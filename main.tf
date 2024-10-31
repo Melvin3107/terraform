4515499938861d0e2db911909686ac876721f39c
mtorresg <mtorresg@miumg.edu.gt>
2024-10-31 00:23:26 -0600

    :bug: Actualizando SSH

--- main.tf
+++ main.tf
@@ -44,47 +44,7 @@ resource "digitalocean_droplet" "web" {
   }
 }
 
-resource "digitalocean_firewall" "web" {
-  name = "only-22-80-and-443"
-  droplet_ids = [digitalocean_droplet.web.id]
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["0.0.0.0/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "icmp"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "udp"
-    port_range            = "53"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "icmp"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
+resource "digitalocean_droplet_firewall" "web_firewall" {
+  droplet_id = digitalocean_droplet.web.id
+  firewall_id = "Firewall"
 }
